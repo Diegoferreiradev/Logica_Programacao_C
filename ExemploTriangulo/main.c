998dd3d1e1c3ba6275a00fae1bcbe0f4ca239c21
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-02-04 03:03:27 -0300

    first commit

---
+++ ExemploTriangulo/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+
+    printf("Informe o valor do lado A: ");
+    scanf("%d", &a);
+    printf("Informe o valor do lado B: ");
+    scanf("%d", &b);
+    printf("Informe o valor do lado C: ");
+    scanf("%d", &c);
+
+
+    return 0;
+}
