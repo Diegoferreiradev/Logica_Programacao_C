82362a9f1ce51190281d284a6a9b5432c2f879bb
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-02-04 03:16:48 -0300

    Ajustes no Triângulo

--- ExemploTriangulo/main.c
+++ ExemploTriangulo/main.c
@@ -12,6 +12,19 @@ int main()
     printf("Informe o valor do lado C: ");
     scanf("%d", &c);
 
+    if((a == b)&& (b == c))
+    {
+        printf("O Triangulo é Equilatero");
+    }
+    else if ((a != b) && (b != c) && (a != c))
+    {
+        printf("O Triangulo é Escaleno");
+    }
+    else
+    {
+        printf("O Triangulo é Isoceles");
+    }
+
 
     return 0;
 }
